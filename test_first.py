b5441bbf6ba1c0d8334dd81220411e8e779780ce
Abhra Kar <abhra.kar@gmail.com>
2023-02-01 16:27:06 +0530

    Update test_first.py

--- test_first.py
+++ test_first.py
@@ -12,5 +12,20 @@ def test_calculation():
     response = requests.get(base_url)
     print(response)
 
+def getRestAPIHeaders(applianceIP, username, password):
+    data = {"username": username, "password": password}
+    url = "https://" + applianceIP + "/rest/auth/login"
+    access_token = requests.post(url, json=data, headers={'Content-Type': 'application/json'}, verify=False).json()[
+        'access_token']
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        'Accept': 'application/json',
+        "Authorization": "Bearer " + access_token
+    }
+    return headers
 
+def test_header():
+    resp = getRestAPIHeaders("10.125.236.31","admin","Dell@12345")
+    print("REsponse-- ")
+    print(resp)
 
